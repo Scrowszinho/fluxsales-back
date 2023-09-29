3c7fd08e2eec9f03e5358203a086dd434277dc56
Gustavo <gustascrows@gmail.com>
2023-09-28 22:27:10 -0300

    feat: adicionando modulo

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserController } from 'src/modules/user/controllers/user.controller';
 import { UserService } from './user.service';
 import { UserRepository } from './repositories/user_repository.prisma';
+import { DatabasesModule } from 'src/shared/database/database.module';
 
 @Module({
-  imports: [],
+  imports: [DatabasesModule],
   controllers: [UserController],
   providers: [UserService, UserRepository],
   exports: [UserService],
